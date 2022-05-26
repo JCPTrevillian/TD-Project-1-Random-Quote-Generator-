b66825b4ec8e19e4dfb00f868195911b384b1e81
Jennifer Trevillian <95890754+JCPTrevillian@users.noreply.github.com>
2022-05-26 12:44:10 -0700

    Add files via upload

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -8,22 +8,65 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * fill in the `quotes` array [] with 5 quotes {};
 ***/
+var quotes = [
 
+  {
+  quote: 'The best judge of whether or not a country is going to develop is how it treats it\'s women.',
+  source: 'President Barack Obama'
+  },
+  
+  {
+  quote: 'Change in all things is sweet.' ,
+  source: 'Aristotle'
+  },
+  
+  {
+  quote: 'I can\'t believe what you say, because I see what you do.',
+  source: 'James Baldwin'
+  },
+  
+  {
+  quote: 'Learning is a weightless treasure you always carry easy.',
+  source: 'Chinese Proverb'
+  },
+  
+  {
+  quote: 'As long as poverty, injustice and gross inequality persist in our world, none of us can truly rest.',
+  source: 'Nelson Mandela',
+  citation: 'Mary Fitzgerald Square, Newtown Johannesburg South Africa',
+  year: '2005'
+  },
+  
+  ];
 
 
 /***
- * `getRandomQuote` function
+ * `getRandomQuote` function through the array, chose by length
 ***/
-
+function getRandomQuote(array) {
+  var quoteIndex = Math.floor(Math.random() * quotes.length);
+  for (var i = 0; i < array.length; i++) {
+    var randomQuote = array[quoteIndex];
+  }
+  return randomQuote;
+}
 
 
 /***
- * `printQuote` function
+ * `printQuote` function, include <p></p>
 ***/
 
 
+function printQuote() {
+  const result = getRandomQuote(quotes);
+  var message = "<p class='quote'>" + result.quote + "</p>" +"<p class='source'>" + result.source + "</p>";
+  document.getElementById('quote-box').innerHTML = message;
+
+} 
+
+console.log(quotes)
 
 /***
  * click event listener for the print quote button
